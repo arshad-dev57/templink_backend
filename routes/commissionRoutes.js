3d4dfdbb94f79a876966cb426d29ae9c3b9a7b28
arshad nasir <qureshiarshad947@gmail.com>
2026-02-28 01:20:27 +0500

    remove the admin history payments

--- routes/commissionRoutes.js
+++ routes/commissionRoutes.js
@@ -5,13 +5,12 @@ const  auth  = require('../middleware/auth_middleware');
 const {
   createCommissionPaymentIntent,
   verifyCommissionPayment,
-  getCommissionHistory
+  
 } = require('../controllers/commissionPaymentController');
 
 // Employer routes
 router.post('/create-payment', auth, createCommissionPaymentIntent);
 router.post('/verify-payment', auth, verifyCommissionPayment);
 // Admin routes
-router.get('/history', auth, getCommissionHistory); // Add admin check
 
 module.exports = router;
